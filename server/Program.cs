244284ed11dfa5fa145d01a5ab04fe827900cde1
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-14 17:07:53 +0100

    Basic article structure.

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,14 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => {
+    const string fencingPath = "./articles/fencing.json";
+    return JsonSerializer.Deserialize<Article>(File.ReadAllText(fencingPath));
+});
 
 app.Run();
+
+record Article(string Title, IEnumerable<Section>? Sections);
+record Section(string Title, string Content, IEnumerable<Section>? SubSection);
