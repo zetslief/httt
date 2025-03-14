dbee28055f7c4c4605045fc4bc895d63c721d938
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-14 17:17:50 +0100

    Basic htlm generation.

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,10 +6,31 @@ var app = builder.Build();
 
 app.MapGet("/", () => {
     const string fencingPath = "./articles/fencing.json";
-    return JsonSerializer.Deserialize<Article>(File.ReadAllText(fencingPath));
+    var article = JsonSerializer.Deserialize<Article>(File.ReadAllText(fencingPath))
+        ?? CreateErrorArticle(fencingPath);
+    return Results.Content(ToHtml(article), "text/html");
 });
 
 app.Run();
+
+static string ToHtml(Article article)
+{
+    var builder = new StringBuilder();
+    builder.AppendLine($"<div>");
+    builder.AppendLine($"\t<h1>{article.Title}</h1>");
+    foreach (var section in article.Sections ?? [])
+    {
+        builder.AppendLine($"\t\t<h2>{section.Title}</h2>");
+        builder.AppendLine($"\t\t<p>{section.Content}</p>");
+    }
+    builder.AppendLine($"</div>");
+    return builder.ToString();
+}
+
+static Article CreateErrorArticle(string articleFilePath) => new(
+    $"Error while generating {articleFilePath}",
+    []
+);
 
 record Article(string Title, IEnumerable<Section>? Sections);
 record Section(string Title, string Content, IEnumerable<Section>? SubSection);
