b52915135836379e04339adc67f3f8bda5f02e04
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-15 14:33:18 +0100

    Add configuraiton.

--- gen/Program.cs
+++ gen/Program.cs
@@ -1,4 +1,15 @@
-﻿using static Gemini.Gemini;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+using static Gemini.Gemini;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var dataFolder = configuration.GetRequiredSection("dataFolder").Value;
+Debug.Assert(dataFolder is not null);
+Console.WriteLine(Path.GetFullPath(dataFolder));
 
 using var client = new HttpClient();
 
