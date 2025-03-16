9b37fb6a63a8d720545e76f8cbaba26e8f273fcf
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-16 14:32:39 +0100

    New schema.

--- gen/Database.cs
+++ gen/Database.cs
@@ -6,6 +6,22 @@ namespace gen;
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
     public DbSet<Topic> Topics { get; set; }
+    public DbSet<TopicSource> TopicSources { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TopicSource>()
+            .HasKey(e => new { e.TopicSourceId, e.Name });
+        modelBuilder.Entity<TopicSource>()
+            .Property(e => e.Name)
+            .HasMaxLength(1024 * 10);
+            
+        modelBuilder.Entity<Topic>()
+            .HasKey(table => new { table.TopicId, table.Name });
+        modelBuilder.Entity<Topic>()
+            .Property(t => t.Name)
+            .HasMaxLength(512);
+    }
 }
 
 public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
@@ -21,6 +37,14 @@ public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
 
 public class Topic
 {
-    public required int TopicId { get; set; }
+    public int TopicId { get; set; }
     public required string Name { get; set; }
+    public required TopicSource Source { get; set; }
+}
+
+public class TopicSource
+{
+    public int TopicSourceId { get; set; }
+    public required string Name { get; set; }
+    public required string Content { get; set; }
 }
--- gen/Program.cs
+++ gen/Program.cs
@@ -10,19 +10,23 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile("appSettings.json")
     .Build();
 
-await using var dbContext = new DataContextFactory().CreateDbContext(args);
-var ts = await dbContext.Topics.Select(t => t).CountAsync();
-Console.WriteLine($"There are {ts} topics in the database.");
-
-Environment.Exit(11);
-
 var dataFolder = configuration.GetRequiredSection("dataFolder").Value;
 Debug.Assert(dataFolder is not null);
 var fullPath = Path.GetFullPath(dataFolder);
 Debug.Assert(fullPath is not null);
-var data = Directory.GetFiles(fullPath, "*.json").First();
-var topics = JsonSerializer.Deserialize<IEnumerable<string>>(File.ReadAllText(data))
-    ?? throw new InvalidOperationException("Failed to deserialize topics");
+var dataFiles = Directory.GetFiles(fullPath, "*.json");
+
+await using var ctx = new DataContextFactory().CreateDbContext(args);
+
+foreach (var dataFile in dataFiles)
+{
+    await WriteTopicFileToDatabaseAsync(ctx, dataFile);
+}
+
+var count = await ctx.Topics.CountAsync();
+Console.WriteLine($"there are {count} known topics.");
+
+/*
 using var client = new HttpClient();
 foreach (var topic in topics)
 {
@@ -30,4 +34,20 @@ foreach (var topic in topics)
     Console.WriteLine(await GenerateTextAsync(client, $"Tell me a little bit about {topic}"));
     Console.WriteLine();
 }
+*/
 
+static async Task<string> WriteTopicFileToDatabaseAsync(DataContext ctx, string file)
+{
+    var fullFileName = Path.GetFullPath(file);
+    if (!Path.Exists(fullFileName)) return $"{fullFileName} does not exist";
+    var fileContent = File.ReadAllText(fullFileName);
+    var topics = JsonSerializer.Deserialize<IEnumerable<string>>(fileContent);
+    if (topics is null) return $"Failed to deserialize file content.\n"
+        + $"File Path: {fullFileName}\n"
+        + $"Content {fileContent}";
+    var source = await ctx.TopicSources.AddAsync(new() { Name = Path.GetFileName(fullFileName), Content = fileContent }); 
+    await ctx.Topics
+        .AddRangeAsync(topics.Select(topic => new Topic() { Name = topic, Source = source.Entity }));
+    await ctx.SaveChangesAsync();
+    return string.Empty;
+}
