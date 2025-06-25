706e25a09416741083d6216e9db663ed18adb3ab
Konstantin Konstantin <zetslief@protonmail.com>
2025-06-25 22:01:55 +0200

    fstr: recursive implementation of the Levenstein Distance algorithm

--- fstr/Program.cs
+++ fstr/Program.cs
@@ -13,6 +13,32 @@ var b = args[1];
 PrintInfo($"a: {a}");
 PrintInfo($"b: {b}");
 
+PrintInfo($"Levenstein distance is {LevensteinDistance(a, b)}");
+
+static int LevensteinDistance(ReadOnlySpan<char> a, ReadOnlySpan<char> b)
+{
+    if (b.Length == 0)
+    {
+        return a.Length;
+    }
+
+    if (a.Length == 0)
+    {
+        return b.Length;
+    }
+
+    if (a[0] == b[0])
+    {
+        return LevensteinDistance(a[1..], b[1..]);
+    }
+
+    var first = LevensteinDistance(a[1..], b);
+    var second = LevensteinDistance(a, b[1..]);
+    var third = LevensteinDistance(a[1..], b[1..]);
+
+    return 1 + Math.Min(first, Math.Min(second, third));
+}
+
 static void PrintInfo(string info)
 {
     Console.ForegroundColor = ConsoleColor.Blue;
