231a9847c89f68af5edcb8c19ad95c0085966293
Konstantin Konstantin <zetslief@protonmail.com>
2025-06-25 21:23:32 +0200

    fstr: add playground project for fuzzy string comparison

---
+++ fstr/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (args.Length < 2)
+{
+    PrintError($"Not enough arguments provided. Received {args.Length}: [{string.Join(',', args)}]. Expected 2.");
+    ShowHelp();
+    Environment.Exit(1);
+}
+
+var a = args[0];
+var b = args[1];
+
+PrintInfo($"a: {a}");
+PrintInfo($"b: {b}");
+
+static void PrintInfo(string info)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"INFO: {info}");
+    Console.ResetColor();
+}
+
+static void PrintError(string error)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"ERROR: {error}");
+    Console.ResetColor();
+}
+
+static void ShowHelp()
+{
+}
